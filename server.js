d41a2c50eb81735d68a29171e0aba595a84451b5
shivm29 <puri46394@gmail.com>
2024-02-16 17:01:46 +0530

    basic authentication system implemented

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import connectDB from "./config/db.js"
+import cors from "cors"
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.js"
+
+dotenv.config();
+
+// database config
+// connects to MongoDB database using Mongoose and returns the database connection object
+connectDB();
+
+// initializing express app
+const app = express();
+
+
+// middlewares
+
+// parses the JSON data and makes it available as JavaScript objects 
+// use the body-parser middleware to parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(express.json())
+//  sits between Express application and the incoming HTTP request, and it logs all incoming requests. useful for debugging and troubleshooting application.
+app.use(morgan('dev'))
+
+// routes 
+app.use("/api/v1/auth", authRoutes)
+
+
+
+
+// listening at home port
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
